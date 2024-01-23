a15c3a7b9dd3e35972271971a0fb869a65cfecda
HenriqueSSan <hsstudentwork@gmail.com>
2024-01-23 10:44:08 -0300

    feat: added redirect from any url to /

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { WidgetHome } from "../interfaces/widget/home";
 import { InitialLayout } from "../interfaces/layout/Initial";
 
@@ -7,6 +7,8 @@ export function Router() {
     <Routes>
       <Route path="/" element={<InitialLayout />}>
         <Route index element={<WidgetHome />} />
+
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
       </Route>
     </Routes>
   );
